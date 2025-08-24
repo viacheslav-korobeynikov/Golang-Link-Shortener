e3b0fa4b5bd57045c8b33119aabfcc2fccc7833f
Viacheslav Korobeynikov <korobeynikovvo@gmail.com>
2025-08-24 20:59:46 +0300

    Добавлен модуль авторизации

--- cmd/main.go
+++ cmd/main.go
@@ -1,5 +1,21 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"net/http"
 
+	"github.com/viacheslav-korobeynikov/Golang-Link-Shortener/internal/auth"
+)
+
+func main() {
+	router := http.NewServeMux() // Роутниг
+	auth.NewAuthHandler(router)
+	//Создание сервера
+	server := http.Server{
+		Addr:    ":8081",
+		Handler: router,
+	}
+
+	fmt.Println("Server is listening on port 8081")
+	server.ListenAndServe() // Подключение к порту
 }
