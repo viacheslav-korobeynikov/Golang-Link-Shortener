cfbf201c8cb92ab80c56f67fd01928e21b1bbbba
Viacheslav Korobeynikov <korobeynikovvo@gmail.com>
2025-08-27 18:36:37 +0300

    Добавлен метод поиска ссылки по ее хэшу и автоматического редиректа по ней

--- internal/link/handler.go
+++ internal/link/handler.go
@@ -23,9 +23,10 @@ func NewLinkHandler(router *http.ServeMux, deps LinkHandlerDeps) {
 	router.HandleFunc("POST /link", handler.CreateLink())
 	router.HandleFunc("PATCH /link/{id}", handler.UpdateLink())
 	router.HandleFunc("DELETE /link/{id}", handler.DeleteLink())
-	router.HandleFunc("GET /{hash}", handler.GetLink())
+	router.HandleFunc("GET /{hash}", handler.GoTo())
 }
 
+// Создание ссылки
 func (handler *LinkHandler) CreateLink() http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		//Получение body из запроса
@@ -46,12 +47,14 @@ func (handler *LinkHandler) CreateLink() http.HandlerFunc {
 	}
 }
 
+// Обновление/редактирование ссылки
 func (handler *LinkHandler) UpdateLink() http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 
 	}
 }
 
+// Удаление
 func (handler *LinkHandler) DeleteLink() http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		id := r.PathValue("id")
@@ -59,8 +62,17 @@ func (handler *LinkHandler) DeleteLink() http.HandlerFunc {
 	}
 }
 
-func (handler *LinkHandler) GetLink() http.HandlerFunc {
+// Метод получения ссылки и редиректа
+func (handler *LinkHandler) GoTo() http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-
+		//Получение динамического значения из URL
+		hash := r.PathValue("hash")
+		link, err := handler.LinkRepository.GetByHash(hash)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusNotFound)
+			return
+		}
+		//Редирект пользователя по сслыке
+		http.Redirect(w, r, link.Url, http.StatusTemporaryRedirect)
 	}
 }
