b4543ee080dbf6f71b72bc600e0d2b355eb63f91
Viacheslav Korobeynikov <korobeynikovvo@gmail.com>
2025-08-30 18:13:23 +0300

    Добавлен middleware для логирования запросов

---
+++ pkg/middlware/common.go
@@ -0,0 +1,13 @@
+package middlware
+
+import "net/http"
+
+type WrapperWriter struct {
+	http.ResponseWriter
+	StatusCode int
+}
+
+func (w *WrapperWriter) WriteHeader(statusCode int) {
+	w.ResponseWriter.WriteHeader(statusCode)
+	w.StatusCode = statusCode
+}
