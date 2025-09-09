03a52b7efe70d5c56ee9746ba7b766854a3697f9
Viacheslav Korobeynikov <korobeynikovvo@gmail.com>
2025-09-09 13:32:03 +0300

    Добавлены методы поления списка ссылок и их количества

--- internal/link/repository.go
+++ internal/link/repository.go
@@ -62,3 +62,24 @@ func (repo *LinkRepository) GetById(id uint) (*Link, error) {
 	}
 	return &link, nil
 }
+
+func (repo *LinkRepository) Count() int64 {
+	var count int64
+	repo.Database.
+		Table("links").
+		Where("deleted_at is null").
+		Count(&count)
+	return count
+}
+
+// Метод получения списка ссылок
+func (repo *LinkRepository) GetLinksList(limit, offset int) []Link {
+	var links []Link
+	repo.Database.
+		Table("links").
+		Where("deleted_at is null").
+		Order("id asc").Limit(limit).
+		Offset(offset).
+		Scan(&links)
+	return links
+}
